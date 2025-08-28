c85bea2eed6bc32b50ae71beea88150ab13110a3
verdant-2 <verdant.education1@gmail.com>
2025-08-27 21:14:06 -0700

    Add files via upload

---
+++ s/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+	prefix: "/s/ultraviolet/",
+	bare: "https://meximathurbanned.global.ssl.fastly.net/tspmo/",
+	encodeUrl: Ultraviolet.codec.xor.encode,
+	decodeUrl: Ultraviolet.codec.xor.decode,
+	handler: "/s/uv/uv.handler.js",
+	bundle: "/s/uv/uv.bundle.js",
+	config: "/s/uv/uv.config.js",
+	sw: "/s/uv/uv.sw.js",
+};
